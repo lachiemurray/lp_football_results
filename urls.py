59d7e2cc8a39e8e34f0c2e0bd95d3cf8ed1c6c4f
Lachlan Murray <ljm505@nas56-32.york.ac.uk>
2013-02-25 12:23:33 +0000

    clone of lp_hello_django

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('',
+
+    url(r'^edition/$', 'lp_hello_django.views.edition'),
+    url(r'^sample/$', 'lp_hello_django.views.sample'),
+    url(r'^validate_config/$', 'lp_hello_django.views.validate_config'),
+    url(r'^meta.json$', 'lp_hello_django.views.meta_json'),
+    url(r'^icon.png$', 'lp_hello_django.views.icon'),
+    
+)
+
+
+
+
