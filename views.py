715d19daa039e6bbd31b07ea446ac9bef7242f54
Lachlan Murray <lachie.murray@gmail.com>
2013-02-26 23:47:49 +0000

    transforming from hello_world to football results

--- views.py
+++ views.py
@@ -1,69 +1,55 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-import dateutil.parser
+from django.conf import settings
+import simplejson
+import urllib2
+from datetime import datetime, timedelta
+import time
 import hashlib
 import json
 
-greetings = {"english" : ["Good morning", "Hello", "Good evening"], 
-    "french" : ["Bonjour", "Bonjour", "Bonsoir"], 
-    "german" : ["Guten morgen", "Hallo", "Guten abend"], 
-    "spanish" : ["Buenos d&#237;as", "Hola", "Buenas noches"], 
-    "portuguese" : ["Bom dia", "Ol&#225;", "Boa noite"], 
-    "italian" : ["Buongiorno", "ciao", "Buonasera"], 
-    "swedish": ["God morgon", "Hall&#229;", "God kv&#228;ll"]}
-
 def edition(request):
-
-    # Check for required vars
-    if not request.GET.get('local_delivery_time',False):
-        return HttpResponse("Error: No local_delivery_time was provided", status=400)
-        
-    if not request.GET.get('lang', False):
-        return HttpResponse("Error: No lang was provided", status=400)
-        
-    if not request.GET.get('name', False):
-        return HttpResponse("No name was provided", status=400)
-
-    # Extract configuration provided by user through BERG Cloud. These options are defined by the JSON in meta.json.
-    date = dateutil.parser.parse(request.GET['local_delivery_time'])
-    if not date.weekday() == 0:
-        return HttpResponse("It is not a Monday", status=406)
+       
+    if not request.GET.get('competition', False):
+        return HttpResponse("Error: No competition was provided", status=400)
     
-    # Get config
-    language = request.GET['lang']
-    name = request.GET['name']
     
-    # Pick a time of day appropriate greeting
-    i = 0
-    if date.hour >= 12 and date.hour <=17:
-        i = 1
-    if (date.hour > 17 and date.hour <=24) or (date.hour >= 0 and date.hour <= 3):
-        i = 2
+    # Get competition
+    competition = request.GET['competition']
+    
+    tomorrow = datetime.now() + timedelta(days=1)
+     
+    # Get fixtures - need good API
 
-    greeting = "%s, %s" % (greetings[language][i], name)
-        
-    # Set the etag to be this content. This means the user will not get the same content twice, 
-    # but if they reset their subscription (with, say, a different language they will get new content 
-    # if they also set their subscription to be in the future)
-    context = { 'greeting' : greeting }
-    response = render(request, 'lp_hello_django/hello_world.html', context)
-    response['ETag'] = hashlib.sha224(language+name+date.strftime('%d%m%Y')).hexdigest()
-
+    # Check if there are games today
+    #if not data
+    #    return HttpResponse("No fixtures today", status=406)
+    
+    context = { 'greeting' : 'blank' }
+    response = render(request, 'lp_football_results/index.html', context)
+    response['ETag'] = hashlib.sha224(competition+datetime.now().strftime('%d%m%Y')).hexdigest()
+    
     return response
 
-
 def sample(request):
-
-    # Sample data
-    language = 'english';
-    name = 'Little Printer';
-    greeting = "%s, %s" % (greetings[language][0], name)
- 
-    # Create response
-    context = {'greeting' : greeting}
-    response = render(request, 'lp_hello_django/hello_world.html', context)
-    response['ETag'] = hashlib.sha224(language+name).hexdigest()
+    
+    competition = 'Premier League'
+    
+    fixtures = [
+    {'team_1':'Arsenal','team_2':'Liverpool','kick_off':'15:00','score_1':'4','score_2':'0','status':'R'},
+    {'team_1':'Bolton','team_2':'Fulham','kick_off':'15:00','score_1':'1','score_2':'3','status':''},
+    {'team_1':'Everton','team_2':'Stoke City  ','kick_off':'15:00','score_1':'3','score_2':'1','status':'R'},
+    {'team_1':'Hull City','team_2':'Newcastle','kick_off':'15:00','score_1':'1','score_2':'1','status':'R'},
+    {'team_1':'Man United','team_2':'Liverpool','kick_off':'15:00','score_1':'1','score_2':'4','status':'R'},
+    {'team_1':'Middlesbrough','team_2':'Portsmouth','kick_off':'15:00','score_1':'1','score_2':'1','status':'R'},
+    {'team_1':'Sunderland','team_2':'Wigan','kick_off':'15:00','score_1':'1','score_2':'2','status':'R'},
+    ]
+    
+    context = { 'competition' : competition, 'fixtures' : fixtures }
+    
+    response = render(request, 'lp_football_results/index.html', context )
+    response['ETag'] = hashlib.sha224(competition+(time.strftime('%d%m%Y'))).hexdigest()
     
     return response
 
@@ -75,21 +61,11 @@ def validate_config(request):
     # Extract config from POST
     user_settings = json.loads(request.POST['config'])
     
-    # If the user did choose a language:
-    if not user_settings.get('lang', None):
+    # If the user did choose a competition:
+    if not user_settings.get('competition', None):
         json_response['valid'] = False
         json_response['errors'].append('Please select a language from the select box.')
 
-    # If the user did not fill in the name option:
-    if not user_settings.get('name', None):
-        json_response['valid'] = False
-        json_response['errors'].append('Please enter your name into the name box.')
-
-    # Is a valid language set?
-    if not user_settings.get('lang', None) in greetings:
-        json_response['valid'] = False
-        json_response['errors'].append("We couldn't find the language you selected (%s) Please select another" % user_settings['lang'])
-    
     # Create response
     response = HttpResponse(json.dumps(json_response), mimetype='application/json')
 
